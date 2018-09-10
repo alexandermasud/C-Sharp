e58195593b5e7720096bd6fb781adedf87ffd243
Alexander Masud <alexandermasud@protonmail.com>
2018-09-10 14:41:15 +0200

    Inlämning 1B klar

---
+++ Assignment1B/Computer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Assignment1B
+{
+    public class Computer
+    {
+
+        private String Brand;
+        private String TempScreenSize;
+        private Int32 ScreenSize;
+        private String TempLaptop;
+        private Boolean Laptop = false;
+
+        public void SetComputer() {
+
+            Console.WriteLine("Ange datorns märke");
+            Brand = Console.ReadLine();
+
+            Console.WriteLine("Ange skärmens storlek");
+            TempScreenSize = Console.ReadLine();
+            Int32.TryParse(TempScreenSize, out ScreenSize);
+
+            Console.WriteLine("Är datorn en laptop (y/n)?");
+            TempLaptop = Console.ReadLine().ToLower();
+            if (TempLaptop == "y")
+            {
+                Laptop = true;
+            }
+        }
+
+        public void GetComputer()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("*****");
+            Console.WriteLine("");
+            Console.WriteLine("Brand: " + Brand);
+            Console.WriteLine("Screensize: " + ScreenSize);
+            Console.WriteLine("Laptop: " + Laptop);
+            Console.WriteLine("");
+            Console.WriteLine("*****");
+
+        }
+    }
+}
---
+++ Assignment1B/ComputerMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assignment1B
+{
+    class ComputerMainClass
+    {
+        public static void Main(string[] args)
+        {
+            var computer = new Computer();
+
+            computer.SetComputer();
+            computer.GetComputer();
+        }
+    }
+}
