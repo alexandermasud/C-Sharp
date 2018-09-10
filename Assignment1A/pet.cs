5097bd2f232208238d90f7069ccb817ca5842b58
Alexander Masud <alexandermasud@protonmail.com>
2018-09-10 13:30:27 +0200

    Inlämning1A klar
    Första delen är klar, men VG delen har jag inte gjort än.

---
+++ Assignment1A/pet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Assignment1A
+{
+    class Pet
+    {
+
+        private String Name;
+        private String TempAge;
+        private Int32 Age;
+        private String TempIsFemale;
+        private Boolean IsFemale;
+
+
+        public void AskTheUser()
+        {
+            Console.WriteLine("Vad heter husdjuret ? ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Hur många år är husdjuret ? ");
+            TempAge = Console.ReadLine();
+            Int32.TryParse(TempAge, out Age);
+
+            Console.WriteLine("Är husdjuret en hona? ");
+            TempIsFemale = Console.ReadLine().ToLower();
+
+            if (TempIsFemale == "y"){
+                IsFemale = true;
+            }
+            else{
+                IsFemale = false;
+            }
+
+            
+        }
+
+        public void PresentTheUser()
+        {
+
+            Console.WriteLine("*****");
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Female: " + IsFemale);
+            Console.WriteLine("*****");
+
+        }
+
+    }
+
+}
+
+
+
---
+++ Assignment1A/petownermain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assignment1A
+{
+    public class PetOwnerMain
+    {
+        public static void Main()
+        {
+            Pet pet = new Pet();
+            {
+                pet.AskTheUser();
+                pet.PresentTheUser();
+
+            }
+        }
+    }
+}
