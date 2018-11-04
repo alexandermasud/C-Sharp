a3568b9f8c8d42601c791ef43ff456ceddf81ad9
alexandermasud <alexandermasud@gmail.com>
2018-11-04 13:43:58 +0100

    Påbörjade party klassen

---
+++ Assignment4/Party.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Party
+    {
+
+        private double costPerCapita;
+        private string[] guestList;
+
+
+        public Party (int maxNumOfGuests)
+        {
+            guestList = new string[maxNumOfGuests];
+        }
+    }
+
+}
