d97a7083d0e0f417a6a8a5427a538dc2642bdbaa
alexandermasud <alexandermasud@gmail.com>
2018-11-04 07:46:39 +0100

    Påbörjade video 3:30

--- Assignment3/BMICalculator.cs
+++ Assignment3/BMICalculator.cs
@@ -8,5 +8,42 @@ namespace Assignment3
 {
     class BMICalculator
     {
+
+        private string name = "No Name";
+        private double height = 0;
+        private double weight = 0;
+        private UnitTypes unit;
     }
+
+    // Gets instance name
+    public string GetName (){
+        return name;
+    }
+
+    // Sets instance name
+    public void SetName(string value){
+
+        // Validates if string has value
+        if (!string.IsNullOrEmpty(value))
+            name = value;
+    }
+
+    // Sets height
+    public void SetHeight(double value)
+    {
+        // validates value is 0 or heigher
+        if (value >= 0)
+            height = value;
+    }
+
+    // Sets unitTypes
+    public unitTypes SettUnit(UnitTypes value)
+    {
+        unit = value;
+    }
+
+
+
+
+    
 }
