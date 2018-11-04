8c44ffa1879e049b44e6406d39d5424cbed0e1c2
Alexander Masud <alexandermasud@gmail.com>
2018-11-04 14:13:56 +0100

    Video 21:00

--- Assignment4/Party.cs
+++ Assignment4/Party.cs
@@ -13,10 +13,107 @@ namespace Assignment4
         private string[] guestList;
 
 
-        public Party (int maxNumOfGuests)
+        public Party(int maxNumOfGuests)
         {
             guestList = new string[maxNumOfGuests];
         }
-    }
 
+        public double CostPerCapita
+        {
+            get
+            {
+                return costPerCapita;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    costPerCapita = value;
+                }
+            }
+        }
+
+        public bool AddNewGuest(string firstName, string lastName)
+        {
+            bool ok = true;
+            int vacantPos = FindVacantPos();
+            if (vacantPos != -1)
+            {
+                guestList[vacantPos] = FullName(firstName, lastName);
+            }
+            else
+            {
+                ok = false;
+
+                return ok;
+            }
+        }
+
+        private string FullName(string firstName, string lastName)
+        {
+            return lastName.ToUpper() + ", " + firstName;
+        }
+
+        private int FindVacantPos()
+        {
+            int vacantPos = -1;
+
+            for (int index = 0; index < guestList.Length; index++)
+            {
+                if (string.IsNullOrEmpty(guestList[index]))
+                {
+                    vacantPos = index;
+                    break;
+                }
+            }
+        }
+
+        private int NumOfGuests()
+        {
+            int numGuests = 0;
+
+            for (int i = 0; i < guestList.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(guestList[i]))
+                {
+                    numGuests++;
+                }
+            }
+        }
+
+        public int Count
+        {
+            get { return NumOfGuests(); }
+        }
+
+        public double CalcTotalCost (){
+            int numOfGuests = NumOfGuests();
+            double totalCost = numOfGuests * costPerCapita;
+            return totalCost;
+        }
+
+        public string[] GetGuestList (){
+            int size = NumOfGuests();
+
+            if(size <= 0){
+                return null;
+            }
+
+            string[] guests = new string[size];
+
+            for (int i = 0, j = 0; i < guestList.Length; i++)
+            {
+                if(!string.IsNullOrEmpty (guestList[i])){
+                    guests[j++] = guestList[i];
+                }
+            }
+            return guests;
+        }
+
+        public bool CheckIndex (int index){
+            return (index >= 0) && (index < guestList.Length);
+        }
+
+
+    }
 }
